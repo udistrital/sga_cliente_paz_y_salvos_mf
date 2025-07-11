dce74bdae96ff1f062bba0ad3ca8fb0bf0b2c529
aalejoz25 <alvaroalejo25@gmail.com>
2025-07-11 11:58:56 -0500

    feat: udistrital/sga_documentacion#575
    actualizacion de roles y env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@
 export const environment = {
     production: false,
     apiUrl: "http://localhost:4221/",
-    // SGA_PAZ_Y_SALVOS_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/evaluacion_docente_mid/v1/',
+    // SGA_PAZ_Y_SALVOS_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/paz_y_salvos_mid/v1/',
     SGA_PAZ_Y_SALVOS_MID_SERVICE: 'http://localhost:8582/v1/',
-    // SGA_PAZ_Y_SALVOS_CRUD_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/evaluacion_docente_mid/v1/',
+    // SGA_PAZ_Y_SALVOS_CRUD_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/paz_y_salvos_mid/v1/',
     SGA_PAZ_Y_SALVOS_CRUD_SERVICE: 'http://localhost:8581/v1/'
 };
