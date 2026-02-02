b88daed1365e2d9adbf1fa3b3f15adf79ffef150
Alvaro Zarabanda <alvaroalejo25@gmail.com>
2026-02-02 14:37:44 -0500

    feature: actualizacion de paz_y_salvos_mf udistrital/sga_documentacion#575
    se añade translate service

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -1,78 +1,89 @@
 import { Injectable } from "@angular/core";
 // @ts-ignore
 import Swal from "sweetalert2/dist/sweetalert2";
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: "root",
 })
 export class AlertService {
-  constructor() { }
+  constructor(private translate: TranslateService) { }
 
   showAlert(title: string, text: string) {
-    Swal.fire({
-      icon: "info",
-      title: title,
-      text: text,
-      confirmButtonText: "Aceptar",
-      customClass: {
-        confirmButton: "alertaConfirmarBoton",
-        cancelButton: "alertaCancelarBoton",
-        icon: "alertaIconoWarn",
-      },
+    this.translate.get(['GLOBAL.aceptar']).subscribe(translations => {
+      Swal.fire({
+        icon: "info",
+        title: title,
+        text: text,
+        confirmButtonText: translations['GLOBAL.aceptar'],
+        customClass: {
+          confirmButton: "alertaConfirmarBoton",
+          cancelButton: "alertaCancelarBoton",
+          icon: "alertaIconoWarn",
+        },
+      });
     });
   }
 
-  showSuccessAlert(text: string, title: string = "Operación exitosa") {
-    Swal.fire({
-      icon: "success",
-      title: title,
-      text: text,
-      confirmButtonText: "Aceptar",
-      customClass: {
-        confirmButton: "alertaConfirmarBoton",
-        cancelButton: "alertaCancelarBoton",
-        icon: "alertaIconoSuccess",
-      },
+  showSuccessAlert(text: string, title?: string) {
+    this.translate.get(['GLOBAL.operacion_exitosa', 'GLOBAL.aceptar']).subscribe(translations => {
+      Swal.fire({
+        icon: "success",
+        title: title || translations['GLOBAL.operacion_exitosa'],
+        text: text,
+        confirmButtonText: translations['GLOBAL.aceptar'],
+        customClass: {
+          confirmButton: "alertaConfirmarBoton",
+          cancelButton: "alertaCancelarBoton",
+          icon: "alertaIconoSuccess",
+        },
+      });
     });
   }
 
   showErrorAlert(text: string) {
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: text,
-      confirmButtonText: "Aceptar",
-      customClass: {
-        confirmButton: "alertaConfirmarBoton",
-        cancelButton: "alertaCancelarBoton",
-      },
+    this.translate.get(['GLOBAL.error', 'GLOBAL.aceptar']).subscribe(translations => {
+      Swal.fire({
+        icon: "error",
+        title: translations['GLOBAL.error'],
+        text: text,
+        confirmButtonText: translations['GLOBAL.aceptar'],
+        customClass: {
+          confirmButton: "alertaConfirmarBoton",
+          cancelButton: "alertaCancelarBoton",
+        },
+      });
     });
   }
 
-  showConfirmAlert(text: string, title: string = "Atención"): Promise<any> {
-    return Swal.fire({
-      title: title,
-      text: text,
-      icon: "warning",
-      showCancelButton: true,
-      cancelButtonText: "Cancelar",
-      confirmButtonText: "Aceptar",
-      customClass: {
-        confirmButton: "alertaConfirmarBoton",
-        cancelButton: "alertaCancelarBoton",
-        icon: "alertaIconoConfirmacion",
-      },
+  showConfirmAlert(text: string, title?: string): Promise<any> {
+    return this.translate.get(['GLOBAL.atencion', 'GLOBAL.aceptar', 'GLOBAL.cancelar']).toPromise().then(translations => {
+      return Swal.fire({
+        title: title || translations['GLOBAL.atencion'],
+        text: text,
+        icon: "warning",
+        showCancelButton: true,
+        cancelButtonText: translations['GLOBAL.cancelar'],
+        confirmButtonText: translations['GLOBAL.aceptar'],
+        customClass: {
+          confirmButton: "alertaConfirmarBoton",
+          cancelButton: "alertaCancelarBoton",
+          icon: "alertaIconoConfirmacion",
+        },
+      });
     });
   }
 
-  showLoading(text: string = "Cargando datos...") {
-    Swal.fire({
-      title: text,
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-      didOpen: () => {
-        Swal.showLoading();
-      },
+  showLoading(text?: string) {
+    this.translate.get('GLOBAL.cargando').subscribe(translation => {
+      Swal.fire({
+        title: text || translation,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
     });
   }
 
