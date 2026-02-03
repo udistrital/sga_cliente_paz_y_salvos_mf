00e270d44579d11d44cddc41d3182afcc9d96e5d
Alvaro Zarabanda <alvaroalejo25@gmail.com>
2026-02-03 09:28:54 -0500

    feature: actualizacion de paz_y_salvos_mf udistrital/sga_documentacion#575
    cambio a oikos crud api v2

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -3,7 +3,7 @@ export const environment = {
     apiUrl: "https://sgapazysalvos.portaloas.udistrital.edu.co/",
     SGA_PAZ_Y_SALVOS_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/paz_y_salvos_mid/v1/',
     SGA_PAZ_Y_SALVOS_CRUD_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/paz_y_salvos_crud/v1/',
-    OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v1/',
+    OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
     TIPO_DEPENDENCIA_FACULTAD: 2,
     TIPO_DEPENDENCIA_PROYECTO_CURRICULAR: 3
 };
